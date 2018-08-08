7b55757a27c29a50358a8875003d463a97fce321
Mateo Navarrete <mnz1601@gmail.com>
2018-08-07 20:48:55 -0400

    linted

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,19 @@ module.exports = {
     '@vue/prettier'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/attributes-order': 'error',
+    'vue/html-quotes': 'error',
+    'vue/no-confusing-v-for-v-if': 'error',
+    'vue/order-in-components': 'error',
+    'vue/this-in-template': 'error',
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-closing-bracket-spacing': 'error',
+    'vue/no-use-v-if-with-v-for': 'error',
+    'vue/no-v-html': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/script-indent': 'error'
   },
   parserOptions: {
     parser: 'babel-eslint'
