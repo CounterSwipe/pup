a389cb4126e1427891cb052f0c61777a07c571ec
Mateo Navarrete <mnz1601@gmail.com>
2018-08-07 21:21:48 -0400

    extract css = false

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    extract: false
+  }
+};
