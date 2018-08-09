ef24d659e131ceb6a90ae0957e940b60a1ead867
Mateo Navarrete <mnz1601@gmail.com>
2018-08-09 16:46:11 -0400

    load textInput async

--- src/components/Playground.vue
+++ src/components/Playground.vue
@@ -3,8 +3,8 @@
     <h1>Welcome to the component playground!</h1>
 
     <select v-model="selectedComponent">
-      <option v-for="(component, index) in componentList" :key="index" :value="component">
-        {{ component.name }}
+      <option v-for="(item, index) in componentList" :key="index" :value="item.component">
+        {{ item.label }}
       </option>
     </select>
 
@@ -16,13 +16,22 @@
 <script>
 import Button from "@/components/dynamic/Button";
 import Header from "@/components/dynamic/Header";
-import TextInput from "@/components/dynamic/TextInput";
+// Comment out the line below, since we will be loading it asynchronously
+// import TextInput from '@/components/dynamic/TextInput'
 
 export default {
   name: "Playground",
   data: function() {
     return {
-      componentList: [Button, Header, TextInput],
+      componentList: [
+        { label: "Button", component: Button },
+        { label: "Header", component: Header },
+        {
+          label: "TextInput",
+          // Async loading!
+          component: () => import("@/components/dynamic/TextInput")
+        }
+      ],
       selectedComponent: null
     };
   }
